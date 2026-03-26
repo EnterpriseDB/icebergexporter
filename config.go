19c3cfb675b943cef1fe9efb68643db7492a9809
Peter Wilson <peter.wilson@enterprisedb.com>
2026-03-26 17:46:34 +1100

    docs: Add licensing

--- config.go
+++ config.go
@@ -1,3 +1,6 @@
+// Copyright 2026- EnterpriseDB
+// SPDX-License-Identifier: Apache-2.0
+
 package icebergexporter
 
 import (
--- internal/arrow/promoted.go
+++ internal/arrow/promoted.go
@@ -1,3 +1,6 @@
+// Copyright 2026- EnterpriseDB
+// SPDX-License-Identifier: Apache-2.0
+
 package arrow
 
 // PromotedAttributes defines which OTel attributes are promoted to top-level
